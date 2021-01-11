17ac5b6d433d035849518cf7cda02a57571e0fa8
To Dat <todat999@gmail.com>
2021-01-11 15:18:37 +0700

    create messahe

--- app/src/main/java/com/example/messagechat/Fragment/AccountFragment.java
+++ app/src/main/java/com/example/messagechat/Fragment/AccountFragment.java
@@ -10,13 +10,15 @@ import android.view.ViewGroup;
 
 import com.example.messagechat.R;
 
+import android.widget.TextView;
 
 public class AccountFragment extends Fragment {
-
+    //private TextView btnLogout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //btnLogout = (TextView) getView().findViewById(R.id.logout);
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
 }
